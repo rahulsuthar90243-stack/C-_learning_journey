b749e71614fa9f4bca62fe9aa7c4f41e8da47bc3
Rahul <rahulsuthar90243@gmail.com>
2026-03-22 11:36:09 +0530

    forEach loop practice

---
+++ chepter_3_loop/forEach.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1, 2 , 3, 4};
+    string names [] = {"Rahul", "Mahipal", "Vp"};
+
+    for(string name : names){
+        cout<<name<<" ";
+    }
+}
